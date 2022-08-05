d62bdf687e2454a2abdb7ab5abd5cc4c9e7d077b
Blazewardog <sailboats9796@gmail.com>
2022-08-04 23:05:24 -0500

    Initial Instant65 Support

---
+++ keyboards/cannonkeys/instant65/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x66, 0x52, 0xEE, 0x67, 0x64, 0x9A, 0x98, 0x18}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4}
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0}
